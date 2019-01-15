9989180b2d09d1215435d8e7f9e8d1de9b3551b7
Nikolas Melgarejo <nikoclops@gmail.com>
2019-01-15 00:56:26 -0500

    adds demo

---
+++ src/components/Demo.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { postToSwapuyoLoginAction } from '../actions/index';
+
+class Demo extends Component {
+  state = {};
+
+  componentDidMount() {
+    console.log('hello');
+    this.props.dispatch(postToSwapuyoLoginAction('niktest', 'password123'));
+  }
+
+  render() {
+    return <div>{this.props.authToken && <Redirect to="/home" />}</div>;
+  }
+}
+
+function mapStateToProps(state) {
+  return { authToken: state.loginReducer.authToken };
+}
+
+export default connect(mapStateToProps)(Demo);
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,74 +1,74 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "../components/Home";
-import Login from "../components/Login";
-import TradeHub from "../components/TradeHub";
-import WantList from "../components/WantList";
-import Register from "../components/Register";
-import RedditTokenRedirectPage from "../components/RedditTokenRedirect/RedditTokenRedirectPage";
-import Header from "../components/Header";
-import SideDrawer from "../components/SideDrawer";
-import Backdrop from "../components/Backdrop/Backdrop.js";
-import { connect } from "react-redux";
-import LandingPage from "../components/LandingPage";
-
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from '../components/Home';
+import Login from '../components/Login';
+import TradeHub from '../components/TradeHub';
+import WantList from '../components/WantList';
+import Register from '../components/Register';
+import RedditTokenRedirectPage from '../components/RedditTokenRedirect/RedditTokenRedirectPage';
+import Header from '../components/Header';
+import SideDrawer from '../components/SideDrawer';
+import Backdrop from '../components/Backdrop/Backdrop.js';
+import { connect } from 'react-redux';
+import LandingPage from '../components/LandingPage';
+import Demo from '../components/Demo';
 //HISTORY CREATED SO I CAN MAKE LANDING PAGE NOT HOLD HEADER
 
 class AppRouter extends Component {
-	state = {
-		sideDrawerOpen: false
-	};
+  state = {
+    sideDrawerOpen: false
+  };
 
-	drawerToggleClickHandler = () => {
-		this.setState(prevState => {
-			return { sideDrawerOpen: !prevState.sideDrawerOpen };
-		});
-	};
+  drawerToggleClickHandler = () => {
+    this.setState(prevState => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
 
-	render() {
-		let backdrop;
-		let header;
+  render() {
+    let backdrop;
+    let header;
 
-		if (this.props.sideDrawerOpen) {
-			backdrop = <Backdrop />;
-		}
-		if (this.props.authToken && this.props.refreshToken) {
-			header = <Header />;
-		}
+    if (this.props.sideDrawerOpen) {
+      backdrop = <Backdrop />;
+    }
+    if (this.props.authToken && this.props.refreshToken) {
+      header = <Header />;
+    }
 
-		return (
-			<BrowserRouter>
-				<React.Fragment>
-					{header}
-					<SideDrawer />
-					{backdrop}
-					<Switch>
-						<Route path="/login" component={Login} exact={true} />
-						<Route path="/register" component={Register} exact={true} />
-						<Route path="/home" component={Home} exact={true} />
-						<Route path="/trade-hub" component={TradeHub} exact={true} />
-						<Route path="/want-list" component={WantList} exact={true} />
-
-						{/* <Route component={NotFound} /> */}
-						<Route
-							path="/RedditTokenRedirect"
-							component={RedditTokenRedirectPage}
-							exact={true}
-						/>
-						<Route path="/" exact={true} component={LandingPage} />
-					</Switch>
-				</React.Fragment>
-			</BrowserRouter>
-		);
-	}
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          {header}
+          <SideDrawer />
+          {backdrop}
+          <Switch>
+            <Route path="/login" component={Login} exact={true} />
+            <Route path="/register" component={Register} exact={true} />
+            <Route path="/home" component={Home} exact={true} />
+            <Route path="/trade-hub" component={TradeHub} exact={true} />
+            <Route path="/want-list" component={WantList} exact={true} />
+            <Route path="/demo" component={Demo} exact={true} />
+            {/* <Route component={NotFound} /> */}
+            <Route
+              path="/RedditTokenRedirect"
+              component={RedditTokenRedirectPage}
+              exact={true}
+            />
+            <Route path="/" exact={true} component={LandingPage} />
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		sideDrawerOpen: state.sideDrawerReducer.sideDrawerOpen,
-		authToken: state.loginReducer.authToken,
-		refreshToken: state.loginReducer.refreshToken
-	};
+  return {
+    sideDrawerOpen: state.sideDrawerReducer.sideDrawerOpen,
+    authToken: state.loginReducer.authToken,
+    refreshToken: state.loginReducer.refreshToken
+  };
 }
 
 export default connect(mapStateToProps)(AppRouter);
